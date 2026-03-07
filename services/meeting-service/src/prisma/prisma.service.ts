8b237f25b583f7cd1d9da16378fc70de4d13f4f5
Shashanth27 <shashanthk27@gmail.com>
2026-03-07 10:43:18 +0530

    chore: remove alternative package manager lock files and fix dev:all script

--- services/meeting-service/src/prisma/prisma.service.ts
+++ services/meeting-service/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client-meeting';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
